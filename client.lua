0ffac08f0cb8b16a0cce7341a89c84656e4764a4
Eyesmack <40875081+Eyesmack@users.noreply.github.com>
2022-04-18 17:38:09 +1200

    Added comments

--- client.lua
+++ client.lua
@@ -1,8 +1,15 @@
 Citizen.CreateThread(function()
 	while true do
+        -- If you don't have this, the mod will crash
 		Citizen.Wait(0)
+
+        -- Setting ped to true allows the ped to shoot "friendlies" [FiveM Natives](https://docs.fivem.net/natives/?_0xB3B1CB349FF9C75D)
 		SetCanAttackFriendly(GetPlayerPed(-1), true, false)
+
+        -- [FiveM Natives](https://docs.fivem.net/natives/?_0xF808475FA571D823)
 		NetworkSetFriendlyFireOption(true)
 	end
+
+    -- Prints to the console
     print("PVP Enabled")
 end)
